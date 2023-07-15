ab22fe9d98d9a425114f149566f027001e1867dc
sfyzsr <sfyzsr@gmail.com>
2023-07-15 10:35:18 +0100

    update

---
+++ 1.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import cv2 as cv
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+# from scipy.interpolate import griddata
+# from scipy.interpolate import RBFInterpolator
+from tqdm import tqdm
+# import open3d as o3d
+import os
+from pyquaternion import Quaternion
+img_file = './2.png'
+img = cv.imread(img_file, cv.IMREAD_GRAYSCALE)
+plt.imshow(img, cmap='gray')
+plt.show()
